6eed8cba211b37a70685a3db295cf4d8505051a8
Pinglei He <pingleihe@outlook.com>
2021-07-30 12:32:46 +0800

    finished lab2

---
+++ lab02/cmake-build-debug/CMakeFiles/lab02.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/pingleihe/Desktop/CS61C/lab02/list.c" "/Users/pingleihe/Desktop/CS61C/lab02/cmake-build-debug/CMakeFiles/lab02.dir/list.c.o"
+  "/Users/pingleihe/Desktop/CS61C/lab02/test_list.c" "/Users/pingleihe/Desktop/CS61C/lab02/cmake-build-debug/CMakeFiles/lab02.dir/test_list.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lab02/cmake-build-debug/CMakeFiles/lab02.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab02.dir/list.c.o"
+  "CMakeFiles/lab02.dir/test_list.c.o"
+  "lab02"
+  "lab02.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
